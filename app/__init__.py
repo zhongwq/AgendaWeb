ea2ff441843a2d0323efdac35c8fcf52166800fd
zhongwq <1316628630@qq.com>
2017-06-14 22:31:00 +0800

    Delete sth useless

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_mail import Mail
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
